b935885b7af9ed702524a32d9430b5a645e9df0b
JK <jaya.kirtani@gmail.com>
2023-01-17 17:10:40 +0000

    Create board with loops

--- src/Board.js
+++ src/Board.js
@@ -23,21 +23,25 @@ export default function Board({ squares, onPlay, isXNext }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onClick={() => handleClick(0)} />
-        <Square value={squares[1]} onClick={() => handleClick(1)} />
-        <Square value={squares[2]} onClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onClick={() => handleClick(3)} />
-        <Square value={squares[4]} onClick={() => handleClick(4)} />
-        <Square value={squares[5]} onClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onClick={() => handleClick(6)} />
-        <Square value={squares[7]} onClick={() => handleClick(7)} />
-        <Square value={squares[8]} onClick={() => handleClick(8)} />
-      </div>
+      {Array(3)
+        .fill("")
+        .map((c, i) => {
+          return (
+            <div className="board-row">
+              {Array(3)
+                .fill()
+                .map((c, j) => {
+                  const pos = i * 3 + j;
+                  return (
+                    <Square
+                      value={squares[pos]}
+                      onClick={() => handleClick(pos)}
+                    />
+                  );
+                })}
+            </div>
+          );
+        })}
     </>
   );
 }
